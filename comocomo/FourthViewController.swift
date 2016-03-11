ea3f10685e4e2810abc7e6e6d3968c29cd2d8361
seojungwon <garden715@gmail.com>
2016-03-11 16:55:25 +0900

    init

--- comocomo/FourthViewController.swift
+++ comocomo/FourthViewController.swift
@@ -1,25 +1,25 @@
 //
-//  FirstViewController.swift
+//  FourthViewController.swift
 //  comocomo
 //
 //  Created by 양윤영 on 2016. 3. 7..
 //  Copyright © 2016년 양윤영. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FourthViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
