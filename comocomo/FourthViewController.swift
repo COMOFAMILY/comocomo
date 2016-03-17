0c5aa8fa7daf703ee19146eb2f497cd5e062ac32
seojungwon <garden715@gmail.com>
2016-03-17 16:11:39 +0900

    forth

--- comocomo/FourthViewController.swift
+++ comocomo/FourthViewController.swift
@@ -10,9 +10,19 @@ import Foundation
 import UIKit
 
 class FourthViewController: UIViewController {
-    
+    @IBOutlet weak var containerViewA: UIView!
+    @IBOutlet weak var containerViewB: UIView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+
+    @IBAction func showSearchResult(sender: AnyObject) {
+        let resultsController = UITableViewController(style: .Plain)
+        let searchController = UISearchController(searchResultsController: resultsController)
+        self.presentViewController(searchController, animated: true, completion: nil)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -21,5 +31,18 @@ class FourthViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func toggle(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            UIView.animateWithDuration(0.5, animations: {
+                self.containerViewA.alpha = 1
+                self.containerViewB.alpha = 0
+            })
+        } else {
+            UIView.animateWithDuration(0.5, animations: {
+                self.containerViewA.alpha = 0
+                self.containerViewB.alpha = 1
+            })
+        }
+    }
     
 }
